73c2f8ef67377174024e1cf3b6eb2772838b1620
pench3r <nageshangzei@gmail.com>
2020-10-23 10:51:34 +0800

    update 84.Largest Rectangle in Histogram

--- cmd/0084.Largest-Rectangle-in-Histogram/Largest Rectangle in Histogram.go
+++ cmd/0084.Largest-Rectangle-in-Histogram/Largest Rectangle in Histogram.go
@@ -1,6 +1,36 @@
 package leetcode
 
+
 func largestRectangleArea(heights []int) int {
 	stack, lenght, maxHeight := []int{}, 0, 0
+	for inx := 0; inx <= len(heights); inx++ {
+		height := 0
+		if inx == len(heights) {
+			height = 0
+		} else {
+			height = heights[inx]
+		}
+		if (len(stack) == 0) || height >= heights[stack[len(stack)-1]] {
+			stack = append(stack, inx)
+		} else {
+			stack_top := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+			if (len(stack) == 0) {
+				lenght = inx
+			} else {
+				lenght = inx - 1 - stack[len(stack)-1]
+			}
+			maxHeight = max(maxHeight, lenght * heights[stack_top])
+			inx--
+		}
+	}
 	return maxHeight
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
---
+++ cmd/0084.Largest-Rectangle-in-Histogram/Largest Rectangle in Histogram_test.go
@@ -0,0 +1,63 @@
+package leetcode
+
+import (
+	"testing";
+	"fmt";
+)
+
+type sample struct {
+	paras
+	ans
+}
+
+type paras struct {
+	nums []int
+}
+
+type ans struct {
+	num int
+}
+
+func Test_TwoSum(t *testing.T) {
+	ss := []sample{
+		{
+			paras{
+				[]int{2, 1, 5, 6, 2, 3},
+			},
+			ans{
+				10,
+			},
+		},
+		{
+			paras{
+				[]int{1},
+			},
+			ans{
+				1,
+			},
+		},
+		{
+			paras{
+				[]int{1,1},
+			},
+			ans{
+				2,
+			},
+		},
+		{
+			// bug1
+			paras{
+				[]int{4, 2, 0, 3, 2, 5},
+			},
+			ans{
+				6,
+			},
+		},
+	}
+
+	for _, s := range ss {
+		p, a := s.paras, s.ans
+		fmt.Printf("[except]: %v,  [output]: %v\n", a, largestRectangleArea(p.nums))
+	}
+
+}
